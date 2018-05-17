b09efc2e8916ab198439415012fdcc6d6cb49ea5
Lucas Holmquist <lholmqui@redhat.com>
2018-05-17 13:52:00 -0400

    feat: start using node.js 10. (#36)
    fixes #35

--- test/integration/greeting-integration-test.js
+++ test/integration/greeting-integration-test.js
@@ -11,7 +11,7 @@ const packagejson = require('../../package.json');
 
 const testEnvironment = rhoaster({
   deploymentName: packagejson.name,
-  dockerImage: 'registry.access.redhat.com/rhoar-nodejs/nodejs-8'
+  dockerImage: 'registry.access.redhat.com/rhoar-nodejs/nodejs-10'
 });
 
 testEnvironment.deploy()
